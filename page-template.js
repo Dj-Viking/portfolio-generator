0cdcc5033fb94a545d93f282ce75a8dde5c7436a
DJ Viking <anders.swedishviking@gmail.com>
2020-07-27 13:35:30 -0400

    figured out the rearrangment of the function to make the featured projects' HTML be written above every project that is not featured, regardless of the order they are entered into the node.js CLI

--- page-template.js
+++ page-template.js
@@ -40,101 +40,63 @@ generateAbout = aboutText => {
 }
 
 generateProjects = projectsArr => {
-    //get array of just featured projects
-    const featuredProjects = projectsArr.filter(project =>{
-        if (project.feature){
-            return true;
-        } else {
-            return false;
-        }
-    });
-    const nonFeaturedProjects = projectsArr.filter(project => {
-        if (!project.feature) {
-            return true;
-        } else {
-            return false;
-        }
-    });
-    const featuredProjectHtmlArr = featuredProjects.map(
-        ({ name, description, languages, link }) => {
-            return `
-            <div class="col-12 mb-2 bg-dark text-light p-3 flex-column">
-                <h3 class="portfolio-item-title text-light">
-                    ${name}
-                </h3>
-                <h5 class="portfolio-languages">
-                    ${languages.join(', ')}
-                </h5>
-                <p>
-                    ${description}
-                </p>
-                <a href="${link}" class="btn mt-auto">
-                    <i class="fab fa-github mr-2"></i>View Project on GitHub
-                </a>
-            </div>
-            `;
-        }
-    );
-    const nonFeaturedProjectHtmlArr = nonFeaturedProjects.map(
-        ({ name, description, languages, link }) => {
-            return `
-                <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-                    <h3 class="portfolio-item-title text-light">
-                        ${name}
-                    </h3>
-                    <h5 class="portfolio-languages">
-                        Built With:${languages.join(', ')}
-                    </h5>
-                    <p>
-                        ${description}
-                    </p>
-                    <a href="${link} class="btn mt-auto">
-                        <i class="fab fa-github mr-2"></i>View Project on GitHub
-                    </a>
-                </div>
-            `;
-        }
-    );
+    // get array of just featured projects
+  
     return `
-                <section class="my-3 id="portfolio">
-                    <h2 class="text-dark bg-primary p-2 display-inline-block">
-                        Work
-                    </h2>
-                    <div class="flex-row justify-space-between">
-                        ${featuredProjectHtmlArr.join('')}
-                        ${nonFeaturedProjectHtmlArr.join('')}
-                    </div>
-                </section>
-    `;
-    
-}
-    const projectHtmlArr = projectsArr.map(({ name, description, languages, link }) => {
-        return `<div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-                        <h3 class="portfolio-item-title text-light">
-                            ${name}
-                        </h3>
-                        <h5 class="portfolio-languages">
-                            ${languages.join(', ')}
-                        </h5>
-                        <p>
-                            ${description}
-                        </p>
-                        <a href="${link}" class="btn mt-auto">
-                            <i class="fab fa-github mr-2"></i>View Project on GitHub
-                        </a>
-                    </div>`;
-    })
-    return `<section class="my-3" id="portfolio">
+            <section class="my-3 id-portfolio">
                 <h2 class="text-dark bg-primary p-2 display-inline-block">
                     Work
                 </h2>
                 <div class="flex-row justify-space-between">
-                    ${projectHtmlArr.join('')}
-                </div>
+                ${projectsArr
+                    .filter(({feature}) => feature)//implicit return value into .map()
+                    .map(({ name, description, languages, link }) => {
+                        return `
+                            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                            <h3 class="portfolio-item-title text-light">
+                                    ${name}
+                                </h3>
+                                <h5 class="portfolio-languages">
+                                    Built With: ${languages.join(', ')}
+                                </h5>
+                                <p>
+                                    ${description}
+                                </p>
+                                <a href="${link}" class="btn mt-auto">
+                                    <i class="fab fa-github mr-2"></i>View Project on GitHub
+                                </a>
+                            </div>
+                        `;
+                    })
+                    .join('')
+                }
+                
+                ${projectsArr
+                    .filter(({ feature }) => !feature)//implicit return value into .map() 
+                    .map(({ name, description, languages, link }) => {
+                        return `
+                            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                                <h3 class="portfolio-item-title text-light">
+                                    ${name}
+                                </h3>
+                                <h5 class="portfolio-languages">
+                                    Built With: ${languages.join(', ')}
+                                </h5>
+                                <p>
+                                    ${description}
+                                </p>
+                                <a href="${link}" class="btn mt-auto">
+                                    <i class="fab fa-github mr-2"></i>View on GitHub
+                                </a>
+                            </div>
+                        `;
+                    })
+                    .join('')
+                }
+                </div>        
             </section>
-    `;
-
-
+        `
+}
 
 //take in the templateData object with all our answers
 module.exports = templateData => {
